ad78caa640d091ee4c7f702f3a82a55f8a8e612b
Brian Thompson <brwthompson14@gmail.com>
2024-01-29 17:33:57 +0000

    Add skills page and basic info

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const name = "Brian Thompson";
 export const title = "Software Developer";
 
 export const navMap = {
-  skills: "/",
+  skills: "/skills",
   values: "/values",
   projects: "/",
   "work history": "/",
