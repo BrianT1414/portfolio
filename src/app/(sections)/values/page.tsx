8d1a5f9818a3832a3b7841a0cd0850c9048e02e9
Brian Thompson <brwthompson14@gmail.com>
2024-01-29 17:16:56 +0000

    Separate out some reusable pieces of section pages, add some basic info to values page

--- src/app/(sections)/values/page.tsx
+++ src/app/(sections)/values/page.tsx
@@ -1,19 +1,24 @@
-import { Typography } from "@mui/material";
+import Template from "../template";
+
+const description =
+  'Rather than provide a list of generic professional values such as "integrity", "accountability", and "respect" (all of which I do value), my goal is to provide a list of specific traits that are very important to me. This list is not limited to character traits, but also extends into the types and methods of work I value.';
 
 export default function Values() {
   return (
-    <div>
-      <div>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Typography>
-      </div>
-    </div>
+    <Template title="Professional Values" description={description}>
+      <ul>
+        <li>Quality</li>
+        <li>Ownership</li>
+        <li>Purpose</li>
+      </ul>
+      <ul>
+        <li>Developer Experience</li>
+        <li>Automation</li>
+        <li>Continuous Improvement</li>
+        <li>Pragmatism</li>
+        <li>Agile</li>
+        <li>Collaboration</li>
+      </ul>
+    </Template>
   );
 }
