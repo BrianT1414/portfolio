b5a91a44d98b6fdcfbaeb6263fe6b4e5ac04002d
Brian Thompson <brwthompson14@gmail.com>
2024-01-31 02:30:13 +0000

    Convert description prop to ReactNode for better IDE readability

--- src/app/(sections)/skills/page.tsx
+++ src/app/(sections)/skills/page.tsx
@@ -2,8 +2,15 @@ import Skill from "@/components/Skill";
 import Template from "../template";
 import { skills } from "./skill_data";
 
-const description =
-  "Below I have provided a list of specific skills I have aquired in my career. A simple list of skills and years of experience only provides the most cursory view of proficiency; so I have also included expandable sections where I describe my experience, anecdotes, and opinions in an attempt to demonstrate my actual competency and experience in each area.";
+const description = (
+  <>
+    Below I have provided a list of specific skills I have aquired in my career.
+    A simple list of skills and years of experience only provides the most
+    cursory view of proficiency; so I have also included expandable sections
+    where I describe my experience, anecdotes, and opinions in an attempt to
+    demonstrate my actual competency and experience in each area.
+  </>
+);
 
 export default function Skills() {
   return (
