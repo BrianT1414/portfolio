d3b746a73a3d5b84da4c0083962486696b63ea7c
Brian Thompson <brwthompson14@gmail.com>
2024-02-03 22:48:54 +0000

    Add MUI theme

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+import { teal } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+  },
+});
+
+export default theme;
